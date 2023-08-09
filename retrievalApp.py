fb20bc71f555bee506b148631583f15e1ccaf3d9
anylots <842567494@qq.com>
2023-08-09 17:18:23 +0800

    test

--- retrievalApp.py
+++ retrievalApp.py
@@ -19,8 +19,7 @@ vector_store = SupabaseVectorStore(
 
 
 
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer,Under the premise of ensuring accuracy, 
-the answers should be as rich as possible.
+prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
 {context}
 
